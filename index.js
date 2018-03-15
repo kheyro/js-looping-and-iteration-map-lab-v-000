ef47d283a40bad0617b1df9e4e0ef0304e551025
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-15 22:11:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@
 function lowerCaseDrivers(arr) {
   return arr.map(function (name) { return name.toLowerCase() })
 }
+
+function nameToAttributes(arr) {
+  return arr.map(
+    function (user) {
+      return Object.assign({}, { firstName: user.split(' ')[0], lastName: user.split(' ')[1]})
+    }
+  )
+}
